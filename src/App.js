f3ad5d8323f80faa8178d674112a7d7c7b326e7d
Denis <stivenrain@gmail.com>
2018-05-22 18:43:42 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import GradientPicker from './container/gradient-picker';
+import ReactGradientPicker from 'components/react-gradient-picker';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <GradientPicker />
+      <ReactGradientPicker />
     );
   }
 }
